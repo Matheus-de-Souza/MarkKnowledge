e632e8a64ac647de3c990e7af360dd14f85f22dc
Matheus de Souza <mws.souza@gmail.com>
2015-12-08 11:49:26 -0200

    Working show file contents method

--- index.php
+++ index.php
@@ -26,6 +26,19 @@ $app->get('/list-files', function () {
     echo json_encode($result);
 });
 
+$app->post('/show-file-contents', function () use ($app) {
+        
+    $body = $app->request->getBody();
+    $body = json_decode($body);
+
+    $filename = $body->file;
+   
+    $result = array ();
+    $result['file-contents'] = file_get_contents($filename);
+
+    echo json_encode($result);
+});
+
 $app->run();
 
 ?>
